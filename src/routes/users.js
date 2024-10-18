ef7550b1ed7f0e8647ec6f09f5de434a6938027a
mario guerra <mario.guerra@coresam.cl>
2024-10-18 12:20:48 -0300

    delete top query

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,7 +8,7 @@ const { sql, poolPromise } = require('../database/conection');
 router.get('/liquidacion/usuarios', async (req, res) => {
   try {
     const pool = await poolPromise;  // Obtener la conexión al pool de SQL Server
-    const result = await pool.request().query(`SELECT TOP (100) RunCuerpo, RunDigito, Nombres, ApellidoPaterno, ApellidoMaterno, Email FROM [Castellano].[dbo].[Persona]`);  // Ejecutar consulta SQL
+    const result = await pool.request().query(`SELECT RunCuerpo, RunDigito, Nombres, ApellidoPaterno, ApellidoMaterno, Email FROM [Castellano].[dbo].[Persona]`);  // Ejecutar consulta SQL
     res.json(result.recordset);  // Enviar los resultados como JSON
   } catch (err) {
     res.status(500).json({ error: err.message });  // Manejo de errores
